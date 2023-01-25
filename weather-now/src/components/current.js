a7948ff1c05182f1dab1d900d11c376934aab599
Backtracker7 <backtracker33@gmail.com>
2023-01-25 22:54:34 +0100

    added simple ui

---
+++ weather-now/src/components/current.js
@@ -0,0 +1,15 @@
+
+
+function Current() {
+    return (
+    <div>
+        <img></img>
+        <h1>City: Belgrade</h1>
+        <h3>Current weather: Cloudy</h3>
+        <h3>Temperature: 3°C</h3>
+        <h3>Precipitation: 20%</h3>
+    </div>
+    );
+}
+
+export default Current;
