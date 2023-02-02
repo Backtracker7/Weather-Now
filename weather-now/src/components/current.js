852f511f2773d91a32dca1929cca334bff6efff9
Backtracker7 <backtracker33@gmail.com>
2023-02-02 02:22:57 +0100

    I just added API to check a temperature and wind speed for Belgrade

--- weather-now/src/components/current.js
+++ weather-now/src/components/current.js
@@ -1,12 +1,30 @@
+import {useEffect, useState} from 'react';
 
 function Current() {
+
+    const [weather, setWeather] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.open-meteo.com/v1/meteofrance?latitude=44.80&longitude=20.47&current_weather=true')
+            .then(response => response.json())
+            .then((data) => {
+            setWeather(data.current_weather);
+            })
+            .catch((err) => {
+                console.log(err.message);
+             });
+    }, []);
+
     return (
     <div>
-        <img src="" alt="" />
-        <h1>City: Belgrade</h1>
-        <h3>Current weather: Cloudy</h3>
-        <h3>Temperature: 3°C</h3>
-        <h3>Precipitation: 20%</h3>
+        <div>
+            <img src="" alt="" />
+            <div className="weather-card">
+                <h3>Weather for Belgrade</h3>
+                <h3 className="temperature">Temperature: {weather.temperature} °C</h3>
+                <h3 className="wind-speed">Wind speed: {weather.windspeed}</h3>
+            </div>
+        </div>
     </div>
     );
 }
